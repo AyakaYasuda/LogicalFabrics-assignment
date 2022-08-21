45908a83c69929618f5f752dad6f5e4ac7d9356e
AyakaYasuda <ayakayasuda23@gmail.com>
2022-08-20 20:18:59 -0700

    added error message

--- assignment-b/components/ImageUrlSubmitForm.tsx
+++ assignment-b/components/ImageUrlSubmitForm.tsx
@@ -5,6 +5,7 @@ import axios from 'axios'
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import FindInPageIcon from '@mui/icons-material/FindInPage'
+import Alert from '@mui/material/Alert'
 
 const baseUrl = process.env.NEXT_PUBLIC_API_ENDPOINT
 
@@ -12,6 +13,7 @@ const ImageUrlSubmitForm = () => {
   const { dispatch } = useContext(analyzedDataCtx)
   const [inputUrl, setInputUrl] = useState<string>('')
   const [analyzing, setAnalyzing] = useState<boolean>(false)
+  const [message, setMessage] = useState<string>('')
 
   const inputUrlHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setInputUrl(e.target.value)
@@ -26,43 +28,52 @@ const ImageUrlSubmitForm = () => {
         dispatch({ type: 'fetch', payload: response.data })
         setAnalyzing(false)
         setInputUrl('')
+        setMessage('')
       })
       .catch((err) => {
         setAnalyzing(false)
+        setMessage(`${err.response.data}! Please try again...`)
       })
   }
 
   return (
-    <div className='flex items-center justify-center gap-x-4'>
-      <TextField
-        required
-        id='image-url'
-        label='Image URL'
-        value={inputUrl}
-        onChange={inputUrlHandler}
-        InputLabelProps={{ shrink: true }}
-        sx={{
-          width: 300,
-          '& .MuiFormLabel-root': {
-            fontSize: 16,
-          },
-          '& .MuiInputBase-root': {
-            height: 45,
-          },
-        }}
-      />
-      <Button
-        variant='contained'
-        size='large'
-        color='primary'
-        disabled={analyzing || !inputUrl}
-        onClick={submitFormHandler}>
-        <>
-          <FindInPageIcon sx={{ marginRight: '2px' }} />
-          {analyzing ? 'ANALYZING...' : 'ANALYZE'}
-        </>
-      </Button>
-    </div>
+    <>
+      <div className='flex items-center justify-center gap-x-4 py-5'>
+        <TextField
+          required
+          id='image-url'
+          label='Image URL'
+          value={inputUrl}
+          onChange={inputUrlHandler}
+          InputLabelProps={{ shrink: true }}
+          sx={{
+            width: 300,
+            '& .MuiFormLabel-root': {
+              fontSize: 16,
+            },
+            '& .MuiInputBase-root': {
+              height: 45,
+            },
+          }}
+        />
+        <Button
+          variant='contained'
+          size='large'
+          color='primary'
+          disabled={analyzing || !inputUrl}
+          onClick={submitFormHandler}>
+          <>
+            <FindInPageIcon sx={{ marginRight: '2px' }} />
+            {analyzing ? 'ANALYZING...' : 'ANALYZE'}
+          </>
+        </Button>
+      </div>
+      {message && (
+        <Alert severity='error' sx={{ width: 450, margin: '0 auto' }}>
+          {message}
+        </Alert>
+      )}
+    </>
   )
 }
 
